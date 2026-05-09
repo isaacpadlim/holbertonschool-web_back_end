96c1ebf7cdef5d675479b3821e917908baa131b2
isaacpadlim <isaac.padilla@anahuac.mx>
2026-05-08 20:07:02 -0600

    Task 4: Create trigger to decrease item quantity

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,9 @@
+-- TASK 4
+-- Script that creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
