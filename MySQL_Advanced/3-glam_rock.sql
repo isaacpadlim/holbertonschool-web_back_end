e9a41fea6bb52f327ac780f12c2e8e72585f9016
isaacpadlim <isaac.padilla@anahuac.mx>
2026-05-08 20:05:14 -0600

    Task 3: Glam rock bands lifespan ranking

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Script that lists all bands with Glam rock as their main style
+-- Ranked by their longevity until 2024
+SELECT band_name, (IFNULL(split, 2024) - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
